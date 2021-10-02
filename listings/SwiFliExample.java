7884c1081c1b834e4fc7d8230c01a700aab8724d
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2021-10-02 12:08:41 +0200

    + add paper on 2p dsl

---
+++ listings/SwiFliExample.java
@@ -0,0 +1,3 @@
+Query query = new Query("parent", new Term[] { new Atom("adam"), new Variable("X") } );
+Map<String,Term> solution = query.oneSolution();
+System.out.println("The child of Adam is " + solution.get("X"));
